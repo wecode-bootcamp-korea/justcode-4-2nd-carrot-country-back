9016a4d5efe764481745f6e10f5bd8e696976677
dami kim <tmkim@sunnywt.com>
2022-04-20 15:07:32 +0900

    [Fix] schema userTable 대소문자 맞춰서 수정

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,11 +2,11 @@ const productService = require("../services/productService");
 
 const createProduct = async (req, res) => {
     try {
-      const { userId, , cityID, districtId }  = req
+      const { userId, cityID, districtId }  = req
+      console.log("할당되는지 찍어보자",userId, cityID, districtId) 
       const { addOptionId, quantity, totalPrice } = req.body;
   
       const createProduct = await productService.createProduct(
-        
         addOptionId,
         quantity,
         totalPrice
