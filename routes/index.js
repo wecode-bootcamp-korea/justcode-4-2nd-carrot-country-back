af6608e5cbab77abc58350274cd0dfa5bee1032f
dami kim <tmkim@sunnywt.com>
2022-04-20 13:59:15 +0900

    [중간저장] branch 이동을 위한 커밋

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router(); // express 라우팅 기능을 사용하기 위해서 router 객체가 필요합니다.
 
-const userRoute = require("./userRoute");
+// const userRoute = require("./userRoute");
 const productRoute = require("./productRoute");
-const infoRoute = require("./infoRoute");
-const chatRoute = require("./chatRoute");
+// const infoRoute = require("./infoRoute");
+// const chatRoute = require("./chatRoute");
 
-router.use("/users", userRoute);
+// router.use("/users", userRoute);
 router.use("/products", productRoute);
-router.use("/infos", infoRoute);
-router.use("/chats", chatRoute);
+// router.use("/infos", infoRoute);
+// router.use("/chats", chatRoute);
 
 module.exports = router; // 이렇게 내보낸 router 는 express app 의 미들웨어로 사용됩니다.
