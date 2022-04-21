25cbde60ba36b3f78ec3fa7e93500bfc44568e3c
geunhong Lim <seelime11@gmail.com>
2022-04-21 19:52:56 +0900

    [add] 동네정보 좋아요등록/취소 API

--- routes/infoRoute.js
+++ routes/infoRoute.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
+const authorization = require("../middleware/authorization");
 const infoController = require("../controllers/infoController");
 
 // GET
 router.get("/", infoController.getInfos);
 router.get("/:infoId", infoController.getInfo);
 router.get("/search/info", infoController.getSearchInfos);
+//POST
+router.post(
+  "/:infoId/like",
+  authorization.getUserIdByVerifyToken,
+  infoController.postInfoLike
+);
+//DELETE
+router.delete(
+  "/:infoId/unlike",
+  authorization.getUserIdByVerifyToken,
+  infoController.deleteInfoLike
+);
 
 module.exports = router;
