0d04185b0ae5c949798eae05eae8788299f011e4
shon97 <wjdgns200297@gmail.com>
2022-04-25 11:31:31 +0900

    FIX: index.js router error

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router(); // express 라우팅 기능을 사용하기 위해서 router 객체가 필요합니다.
 
 const userRoute = require("./userRoute");
-// const productRoute = require("./productRoute");
+const productRoute = require("./productRoute");
 const infoRoute = require("./infoRoute");
-// const chatRoute = require("./chatRoute");
+const chatRoute = require("./chatRoute");
 const areaRoute = require("./areaRoute");
 const categoryRoute = require("./categoryRoute");
 router.use("/users", userRoute);
